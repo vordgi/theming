5d048076c2aad341708a9a4509e97ef89fa54c3c
Vordgi <sasha2822222@gmail.com>
2021-12-25 10:45:05 +0200

    T-2 Add deployment actions

--- spa/src/components/organisms/MockBlock/index.tsx
+++ spa/src/components/organisms/MockBlock/index.tsx
@@ -6,7 +6,7 @@ const MockBlock: React.FC<{ mockData: string[] }> = ({ mockData }) => (
       <img src="/new-year.jpg" alt="new year" width="200" height="160" />
     </div>
     <h1>
-      List of 200 items
+      List of 500 items
     </h1>
     {mockData.map((title) => (
       <div key={title}>
--- ssg/src/components/organisms/MockBlock/index.tsx
+++ ssg/src/components/organisms/MockBlock/index.tsx
@@ -7,7 +7,7 @@ const MockBlock: React.FC<{ mockData: string[] }> = ({ mockData }) => (
       <Image src="/new-year.jpg" layout="fixed" width="200" height="160" />
     </div>
     <h1>
-      List of 200 items
+      List of 500 items
     </h1>
     {mockData.map((title) => (
       <div key={title}>
