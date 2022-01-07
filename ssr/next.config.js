4ab81f53fbd45613052d629af36f927bc9118970
Vordgi <sasha2822222@gmail.com>
2022-01-07 22:23:42 +0200

    T-6 Add headers for dark mode detection

--- ssr/next.config.js
+++ ssr/next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Accept-CH',
+            value: 'Sec-CH-Prefers-Color-Scheme',
+          },
+          {
+            key: 'Vary',
+            value: 'Sec-CH-Prefers-Color-Scheme',
+          },
+          {
+            key: 'Critical-CH',
+            value: 'Sec-CH-Prefers-Color-Scheme',
+          },
+        ],
+      },
+    ]
+  },
 }
